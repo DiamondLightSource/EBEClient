5cc4e58ca19914fb93619f17d0799537d7bf8920
Gary Yendell <gary.yendell@diamond.ac.uk>
2018-02-15 16:35:19 +0000

    Fix timeout on recv and handle errors appropriately

--- ebe/ebeclient.py
+++ ebe/ebeclient.py
@@ -1,8 +1,7 @@
 """A class to control an EBE-4."""
 
-from time import sleep
 import re
-from socket import socket, AF_INET, SOCK_DGRAM
+from socket import socket, AF_INET, SOCK_DGRAM, timeout
 import logging
 
 
@@ -10,8 +9,7 @@ class EBEClient(object):
 
     """A class to control an EBE-4."""
 
-    TIMEOUT = 5
-    POLL_RATE = 0.5
+    TIMEOUT = 3
     COMMAND_TEMPLATE = "?1234 {command}\n"
     GET_TEMPLATE = COMMAND_TEMPLATE.format(
         command="GetParaValue {param}")
@@ -37,6 +35,7 @@ class EBEClient(object):
 
         self._socket = socket(AF_INET,     # Internet
                               SOCK_DGRAM)  # UDP
+        self._socket.settimeout(self.TIMEOUT)
         self._socket.connect(self._server)
 
     def __del__(self):
@@ -50,41 +49,45 @@ class EBEClient(object):
 
         self._socket.sendto(message, self._server)
         self.logger.debug("Waiting for response...")
-        timer = 0
-        while timer < self.TIMEOUT:
-            data, address = self._socket.recvfrom(1024)
 
+        try:
+            data, address = self._socket.recvfrom(1024)
             if data:
                 self.logger.debug("Received %s bytes from %s",
                                   len(data), address)
                 self.logger.debug("Response: %s", data)
                 return data
-
-            timer += self.POLL_RATE
-            sleep(self.POLL_RATE)
-        self.logger.error("Receive loop timed out")
+            else:
+                self.logger.error("Received empty response")
+        except timeout:
+            self.logger.error("Receive loop timed out")
 
     def get(self, param):
         self.logger.debug("Sending GET request for: %d", param)
         response = self._send(self.GET_TEMPLATE.format(param=param))
-        value = self._validate_response(response, param)
-        if value is not None:
-            return value
-        else:
-            raise IOError("Get failed on param %s - received null value" %
-                          param)
+        if response is not None:
+            value = self._validate_response(response, param)
+            if value is not None:
+                return value
+
+        raise IOError("Get failed on param %s" % param)
 
     def set(self, param, value):
         self.logger.debug("Sending SET request: %d = %s", param, str(value))
         response = self._send(self.SET_TEMPLATE.format(param=param,
                                                        value=value))
-        new_value = self._validate_response(response, param)
-        if new_value == value:
-            self.logger.debug("Param %s successfully set to %s", param, value)
-        else:
-            raise IOError("Set failed on param %s\n"
-                          "Requested value: %s, Returned value: %s" %
-                          (param, value, new_value))
+        if response is not None:
+            new_value = self._validate_response(response, param)
+            if new_value == value:
+                self.logger.debug("Param %s successfully set to %s",
+                                  param, value)
+                return new_value
+            else:
+                self.logger.error(
+                    "Value not set to request value: %s, Returned value: %s",
+                    value, new_value)
+
+        raise IOError("Set failed on param %s" % param)
 
     def _validate_response(self, response, expected_param):
         match = re.match(self.RESPONSE_REGEX, response)
@@ -94,3 +97,5 @@ class EBEClient(object):
             value = value.strip(" ")
             if param == expected_param:
                 return value
+            else:
+                self.logger.error("Failed to match response to request")
